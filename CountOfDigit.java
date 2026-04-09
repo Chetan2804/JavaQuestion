ec79a286804653ed7e3cddba9b604db82b565f02
Chetan2804 <86292663+Chetan2804@users.noreply.github.com>
2026-04-09 14:13:00 +0530

    Added the CountOfDigit.java file

---
+++ CountOfDigit.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class CountOfDigit {
+
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println("Enter a number :");
+
+		int num = s.nextInt();
+
+		int count = 0;
+
+		while (num > 0) {
+
+			num = num / 10;
+			count++;
+
+		}
+
+		System.out.println("Number of digit is : " + count );
+		
+	}
+
+}
